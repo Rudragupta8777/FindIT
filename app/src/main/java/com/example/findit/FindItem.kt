3561796652bdc8f0329c9d4779ea67bd8d35a96d
Rudragupta8777 <rudragupta1204@gmail.com>
2025-05-02 20:36:29 +0530

    Item named accordingly

--- app/src/main/java/com/example/findit/FindItem.kt
+++ app/src/main/java/com/example/findit/FindItem.kt
@@ -5,18 +5,13 @@ import android.os.Bundle
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.button.MaterialButton
 
-class FindLostItem : AppCompatActivity() {
+class FindItem : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_find_lost_item)
-        val submit = findViewById<MaterialButton>(R.id.add_lost)
-        submit.setOnClickListener {
-            val intent = Intent(this, LostItemAddedSuccesfully::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_find_item)
+
         val home = findViewById<ImageView>(R.id.btn_home)
         home.setOnClickListener {
             val intent = Intent(this, HomeScreen::class.java)
--- app/src/main/java/com/example/findit/ReportLostItem.kt
+++ app/src/main/java/com/example/findit/ReportLostItem.kt
@@ -5,13 +5,18 @@ import android.os.Bundle
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 
-class LostItemAddedSuccesfully : AppCompatActivity() {
+class ReportLostItem : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_lost_item_added_succesfully)
-
+        setContentView(R.layout.activity_report_lost_item)
+        val submit = findViewById<MaterialButton>(R.id.add_lost)
+        submit.setOnClickListener {
+            val intent = Intent(this, LostItemReportedSuccesfully::class.java)
+            startActivity(intent)
+        }
         val home = findViewById<ImageView>(R.id.btn_home)
         home.setOnClickListener {
             val intent = Intent(this, HomeScreen::class.java)
